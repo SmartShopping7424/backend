a55a2750c2434b1321cb96fabb451c39201ffe3f
Abhay Upadhyay <61179365+Abhayaku@users.noreply.github.com>
2023-01-10 21:48:30 +0530

    add logs

--- src/handlers/login/otp/otp.js
+++ src/handlers/login/otp/otp.js
@@ -14,66 +14,79 @@ const {
 const { fetch_otp, insert_user_in_customer } = require("./queries");
 
 module.exports.otp = async (req, res) => {
-  // assign inputs
-  const inputs = req.body;
+  try {
+    logger.info("inside otp function");
 
-  // if no attribute found
-  if (Object.getOwnPropertyNames(inputs).length == 0) {
-    return failure(
-      400,
-      "Invalid payload, Request should contain atleast one attribute.",
-      res
-    );
-  }
+    // assign inputs
+    const inputs = req.body;
 
-  // validate payload
-  const errors = await otp_validator(inputs);
-  if (Object.keys(errors).length > 0 && errors.constructor === Object) {
-    return validation_faliure(
-      422,
-      "The request should not contain invalid data.",
-      errors,
-      res
-    );
-  }
-
-  // fetch otp from database
-  const result = parseResponse(
-    await DBService.executeStatement(fetch_otp(inputs.mobile))
-  );
-
-  // if no result found return faliure
-  if (!result) {
-    return failure(400, "Invalid request.", res);
-  }
-
-  // if otp does not match return faliure
-  if (result.otp != inputs.otp) {
-    return failure(400, "Invalid otp.", res);
-  }
-
-  // if mode is customer then insert user in customer
-  if (inputs.mode == "customer") {
-    await DBService.executeStatement(insert_user_in_customer(inputs.mobile));
-  }
-
-  // if otp matches then generate the jwt token
-  jwt.sign(
-    inputs,
-    config.secret_key,
-    { expiresIn: config.key_expiry },
-    (err, token) => {
-      if (err) {
-        logger.info(`Error while jwt sign ::: ${err}`);
-        return failure(400, "Internal server error, Try again later.", res);
-      } else {
-        logger.info(`Successful login`);
-        const response = {
-          message: "Login successful.",
-          token: token,
-        };
-        return success(200, response, res);
-      }
+    // if no attribute found
+    if (Object.getOwnPropertyNames(inputs).length == 0) {
+      return failure(
+        400,
+        "Invalid payload, Request should contain atleast one attribute.",
+        res
+      );
     }
-  );
+
+    // validate payload
+    const errors = await otp_validator(inputs);
+    if (Object.keys(errors).length > 0 && errors.constructor === Object) {
+      return validation_faliure(
+        422,
+        "The request should not contain invalid data.",
+        errors,
+        res
+      );
+    }
+
+    logger.info("all validation pass");
+
+    // fetch otp from database
+    const result = parseResponse(
+      await DBService.executeStatement(fetch_otp(inputs.mobile))
+    );
+
+    // if no result found return faliure
+    if (!result) {
+      return failure(400, "Invalid request.", res);
+    }
+
+    // if otp does not match return faliure
+    if (result.otp != inputs.otp) {
+      return failure(400, "Invalid otp.", res);
+    }
+
+    logger.info("otp has been matched");
+
+    // if mode is customer then insert user in customer
+    if (inputs.mode == "customer") {
+      await DBService.executeStatement(insert_user_in_customer(inputs.mobile));
+    }
+
+    logger.info("customer has been created");
+
+    // if otp matches then generate the jwt token
+    jwt.sign(
+      inputs,
+      config.secret_key,
+      { expiresIn: config.key_expiry },
+      (err, token) => {
+        if (err) {
+          logger.info(`Error while jwt sign ::: ${err}`);
+          return failure(400, "Internal server error, Try again later.", res);
+        } else {
+          logger.info(`Successful login`);
+          const response = {
+            message: "Login successful.",
+            token: token,
+          };
+          return success(200, response, res);
+        }
+      }
+    );
+  } catch (e) {
+    logger.info("error in catch");
+    logger.error(e);
+  }
 };
