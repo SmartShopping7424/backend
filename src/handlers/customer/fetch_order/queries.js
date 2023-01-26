d8309c0d23119b205b3dc7d4fb123f3b3062bf49
Abhay Upadhyay <61179365+Abhayaku@users.noreply.github.com>
2023-01-26 16:09:33 +0530

    update order query

--- src/handlers/customer/fetch_order/queries.js
+++ src/handlers/customer/fetch_order/queries.js
@@ -6,6 +6,6 @@ module.exports = {
 
   // fetch customer order
   fetch_customer_orders(mobile, limit, offset) {
-    return `SELECT * FROM orders WHERE customer_mobile="${mobile}" LIMIT ${limit} OFFSET ${offset};`;
+    return `SELECT * FROM orders WHERE customer_mobile="${mobile}" ORDER BY created_at DESC LIMIT ${limit} OFFSET ${offset};`;
   },
 };
